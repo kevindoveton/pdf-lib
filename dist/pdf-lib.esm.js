d521d377a4d5a4cf52398261ced77d042fccf8b2
Kevin Doveton <kevin.doveton@eckermanns.com.au>
2025-02-03 16:28:24 +1030

    update package

--- dist/pdf-lib.esm.js
+++ dist/pdf-lib.esm.js
@@ -28577,7 +28577,8 @@ const drawRectangle = (options) => {
             `C 0,${ry * (1 - KAPPA)} ${rx * (1 - KAPPA)},0 ${rx},0`,
             `Z`,
         ].join(' ')
-        : `M 0,0 V ${h} H ${w} H 0 Z`;
+        : `M 0,0 V ${h} H ${w} V 0 Z`;
+    console.log(d);
     // the drawRectangle applies the rotation around its anchor point (bottom-left), it means that the translation should be applied before the rotation
     // invert the y parameter because transformationToMatrix expects parameters from an svg space. The same is valid for rotate and ySkew
     let fullMatrix = combineMatrix(matrix || identityMatrix, transformationToMatrix('translate', [x, -y]));
